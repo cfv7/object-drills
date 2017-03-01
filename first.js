5532434bd68845e25f8a38f52361d1aaf57a0e2c
Peter Szujewski <pszujewski@gmail.com>
2017-03-01 14:58:40 -0500

    working on objects from gist file

--- first.js
+++ first.js
@@ -39,10 +39,10 @@ function updateObject(obj) {
 
 console.log(updateObject(emptyObj))
 
-// Modify the personMaker.fullName to be a function that uses self reference 
+// Modify the personMaker.fullName to be a function that uses self reference
 // (via this) in order to return the firstName and lastName properties separated by a space.
 
-// So, for instance, if firstName was 'Jane' and lastName was 'Doe', 
+// So, for instance, if firstName was 'Jane' and lastName was 'Doe',
 // fullName() should return 'Jane Doe'.
 
 function personMaker() {
@@ -68,7 +68,7 @@ function keyDeleter(obj) {
 	for (var key in obj){
 		if (obj[key] === 'foo' || obj[key] === 'bar'){
 			delete obj[key];
-		}		
+		}
 	}
 	return obj;
 }
@@ -83,9 +83,55 @@ var sampleObj = {
   rip: 'rip'
 };
 
-console.log(keyDeleter(sampleObj))
+console.log(keyDeleter(sampleObj));
 
+// Object Drills 2
+// Object Initializers and methods
+// Create an object called loaf using an object initializer ({}) with two properties: flour, which should be set to 300 and water which should be set to 210.
+// Use console.log to print the flour and water properties.
+// Add an empty method to the loaf object called hydration.
+// Fill in the body of the method to return the hydration of the loaf (the water divided by the flour multiplied by 100).
+// Call the hydration method and use console.log to print the result.
 
+var loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return this.water/this.flour * 100;
+  }
+};
 
+//console.log(loaf.flour, loaf.water);
+console.log(loaf.hydration());
 
+// Object's properties
+// Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
+// Loop over the object using for ... in
+// Use console.log to show each property name and its associated value.
 
+function looper(obj) {
+  for (var prop in obj) {
+    console.log(prop+" "+obj[prop]);
+  }
+}
+
+var obj = {
+  foo: 1,
+  bar: 2,
+  fum: 3,
+  quux: 4,
+  spam: 5
+};
+
+looper(obj);
+
+// Arrays in Objects
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+var hobbitStuff = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
+};
+
+console.log(hobbitStuff.meals[3]);
